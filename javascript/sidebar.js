a32e60b5472e3fa629aeee52eb42db1832d72d26
DanielKoltze <dani64h6@stud.kea.dk>
2022-11-22 16:37:19 +0100

    sidebar virker

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,16 +1,47 @@
 const sidebarButton = document.querySelector('.sidebarButton')
 const sideBar = document.querySelector('.sidebar')
 const navBar = document.getElementById('navBar')
-const rightNav = document.querySelector('.rightNav')
+const leftNav = document.querySelector('.leftNav')
+let isClosed = true
 
 sidebarButton.addEventListener('click', e => {
-    if (sideBar.style.width == 0) {
+    if (isClosed) {
         sideBar.classList.add('sidebar-open')
         navBar.classList.add('navBar-open')
-        rightNav.classList.add('rightNav-open')
+        leftNav.classList.add('leftNav-open')
+        sideBar.classList.remove('sidebar-close')
+        navBar.classList.remove('navBar-close')
+        leftNav.classList.remove('leftNav-close')
+        isClosed = false
+        hej()
     }else{
         sideBar.classList.remove('sidebar-open')
         navBar.classList.remove('navBar-open')
+        leftNav.classList.remove('leftNav-open')
+        sideBar.classList.add('sidebar-close')
+        navBar.classList.add('navBar-close')
+        leftNav.classList.add('leftNav-close')
+        isClosed = true
     }
 
 })
+
+function hej(){
+    if(isClosed === false && sideBar.getBoundingClientRect().right < trigger.offsetX){
+        document.addEventListener('click', e => {
+  
+       
+            sideBar.classList.remove('sidebar-open')
+            navBar.classList.remove('navBar-open')
+            leftNav.classList.remove('leftNav-open')
+            sideBar.classList.add('sidebar-close')
+            navBar.classList.add('navBar-close')
+            leftNav.classList.add('leftNav-close')
+            isClosed = true
+        
+    })
+
+    }
+
+
+}
