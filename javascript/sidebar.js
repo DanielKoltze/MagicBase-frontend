fb41ce0f4b4faa2fd50cb88f015130f786e93342
DanielKoltze <dani64h6@stud.kea.dk>
2022-11-22 15:06:40 +0100

    navbar

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,9 +1,16 @@
 const sidebarButton = document.querySelector('.sidebarButton')
 const sideBar = document.querySelector('.sidebar')
+const navBar = document.getElementById('navBar')
+const rightNav = document.querySelector('.rightNav')
 
 sidebarButton.addEventListener('click', e => {
-    if(sideBar.style.width == 0){
+    if (sideBar.style.width == 0) {
         sideBar.classList.add('sidebar-open')
+        navBar.classList.add('navBar-open')
+        rightNav.classList.add('rightNav-open')
+    }else{
+        sideBar.classList.remove('sidebar-open')
+        navBar.classList.remove('navBar-open')
     }
-    
+
 })
