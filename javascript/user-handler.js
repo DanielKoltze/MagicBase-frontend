bf48da5b5fc4be1800585d6d590be29b016dcf48
DanielKoltze <dani64h6@stud.kea.dk>
2022-11-23 13:08:50 +0100

    mangler bare lige at ordne fejlbeskeder

--- javascript/user-handler.js
+++ javascript/user-handler.js
@@ -1,15 +1,34 @@
 
 
- async function getUser(userName){
+let loggedInUser = null
+login()
+function login(){
+    document.querySelector('.loginBtn').addEventListener('click', e => {
+        const username = document.querySelector('.logInUsernameInput').value
+        const password = document.querySelector('.logInPasswordInput').value
+        const user = getUser(username)
+        user.then(userData => {
+          console.log(userData)
+          if(userData.password === password){
+            //hvis brugernavn og password er korrekt
+            loggedInUser = userData
+          }else{
+            //password ikke er korrekt
+          }
+        }).catch(error => {
+            //brugeren fandtes ikke gør noget
+        })
+        
+    })
+}
+
+ function getUser(userName){
     const settings = {
         method: 'GET',
        }
-    makeRequest(`${BASE_URL}/user/${userName}`, settings)
-    .then(
-        (user) => {
-            return user;
-        }
-    )
+       
+       return makeRequest(`${BASE_URL}/user/${userName}`, settings)
+   
     }
 
     async function createUser(user){
