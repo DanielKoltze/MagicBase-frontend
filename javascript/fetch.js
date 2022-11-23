d70a9c89ca0f8565602d99c4769939e7409262e5
masp1234 <mart2c04@stud.kea.dk>
2022-11-24 00:15:35 +0100

    createUser working

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,10 +1,7 @@
-const BASE_URL="http://localhost:8080/api"
+const BASE_URL = "http://localhost:8080/api";
 
-
-const  makeRequest = async (url, settings) => {
-const response = await fetch(url, settings);
-const data = await response.json();
-return data;
-}
-
-
+const makeRequest = async (url, settings) => {
+  const response = await fetch(url, settings);
+  const data = await response.json();
+  return data;
+};
--- javascript/router.js
+++ javascript/router.js
@@ -1,55 +1,50 @@
-let routes = {}
-let templates = {}
+let routes = {};
+let templates = {};
 
-
-const appDiv = document.getElementById('container');
+const appDiv = document.getElementById("container");
 
 function route(path, template) {
-    if (typeof template === 'function') {
-        return routes[path] = template;
-    }
-    else if (typeof template === 'string') {
-        return routes[path] = templates[template];
-    }
-    else {
-        return;
-    }
-
+  if (typeof template === "function") {
+    return (routes[path] = template);
+  } else if (typeof template === "string") {
+    return (routes[path] = templates[template]);
+  } else {
+    return;
+  }
 }
 function template(name, templateFunction) {
-    return templates[name] = templateFunction
+  return (templates[name] = templateFunction);
 }
 
-template('home', () => {
-    home();
-})
-template('my collection', () => {
-    about();
-})
-template('inspiration', () => {
-    admin();
-})
+template("home", () => {
+  home();
+});
+template("my collection", () => {
+  about();
+});
+template("inspiration", () => {
+  admin();
+});
 
-route('/admin', 'admin');
-route('/', 'home');
-route('/about', 'about');
+route("/admin", "admin");
+route("/", "home");
+route("/about", "about");
 
 function resolveRoute(route) {
-    try {
-        return routes[route];
-    }
-    catch (e) {
-        throw new Error(`Route ${route} not found`)
-    }
+  try {
+    return routes[route];
+  } catch (e) {
+    throw new Error(`Route ${route} not found`);
+  }
 }
 
 function router(evt) {
-    let url = window.location.hash.slice(1) || '/';
-    let route = resolveRoute(url);
+  let url = window.location.hash.slice(1) || "/";
+  let route = resolveRoute(url);
 
-    // kører den metode den lige har fået gemt ovenover, som f.eks. vil være () => { home();} i starten 
-    route();
+  // kører den metode den lige har fået gemt ovenover, som f.eks. vil være () => { home();} i starten
+  route();
 }
 
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
