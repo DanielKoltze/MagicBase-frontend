e9ca25caaec049fcefb4221f208339d53e042a15
masp1234 <mart2c04@stud.kea.dk>
2022-11-25 10:35:41 +0100

    router fixed

--- javascript/handlers/collectionHandler.js
+++ javascript/handlers/collectionHandler.js
@@ -1,12 +1,13 @@
 function collection(){
+    console.log("Har routet til collection")
     if(loggedInUser == null){
         window.location.href = DEFAULT_ROUTE; 
     }
+    
     const template = document.getElementById('my-collection')
     const clone = template.content.cloneNode(true)
+    
+    sidebarDeckContainer = document.getElementById('container');
+    showDecks(sidebarDeckContainer, displayInSidebar)
 
-
-    showDecks(container, displayInSidebar)
-
-    appDiv.replaceChildren(clone)
 }
