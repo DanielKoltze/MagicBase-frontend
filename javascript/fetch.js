0fdb62f622932173157ba0d1db1f8ad6fd43cc04
masp1234 <mart2c04@stud.kea.dk>
2022-12-16 11:37:01 +0100

    changed BASE_URL

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://magicbase.azurewebsites.net/api";
 
 const makeRequest = async (url, settings) => {
   const response = await fetch(url, settings);
